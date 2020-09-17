bda7cb1b3548800d209b2f318e267a26f597e1f8
Jacob Bednarz <jacob.bednarz@gmail.com>
2020-09-17 10:03:27 +1000

    add Cask

---
+++ Casks/cf-vault.rb
@@ -0,0 +1,11 @@
+cask "cf-vault" do
+  version "0.0.4"
+  sha256 "3052f9cfb480e5f75bfc5478e03a258b4d45ac221fc78faffb89d14c2e10baa8"
+
+  url "https://github.com/jacobbednarz/cf-vault/releases/download/#{version}/cf-vault_#{version}_darwin_amd64.tar.gz"
+  appcast "https://github.com/jacobbednarz/cf-vault/releases.atom"
+  name "cf-vault"
+  homepage "https://github.com/jacobbednarz/cf-vault"
+
+  binary "cf-vault"
+end
