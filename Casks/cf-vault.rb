28c90f5637c9a582b557c6c9400e0467d7789067
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-01-15 06:51:04 +1100

    bump to 0.0.11

--- Casks/cf-vault.rb
+++ Casks/cf-vault.rb
@@ -1,6 +1,6 @@
 cask "cf-vault" do
-  version "0.0.10"
-  sha256 "7c7858c29c39fa1a61b9efe7c9f92746a2780927e6e08a7990f71101047f4470"
+  version "0.0.11"
+  sha256 "b2c9d8c35d245d9e4cbbf865f2edd927109df057037f97a699a7aa041c125575"
 
   url "https://github.com/jacobbednarz/cf-vault/releases/download/#{version}/cf-vault_#{version}_darwin_amd64.tar.gz"
   appcast "https://github.com/jacobbednarz/cf-vault/releases.atom"
